29d2ff2592cd9e319918fba3dcd41327d4b4fabf
Cindy <ong.wenling.cindy@dhs.sg>
2018-11-02 16:07:30 +0800

    Fixed bug in contribution_each_account.py

--- cpfcalui/contribution_each_account.py
+++ cpfcalui/contribution_each_account.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 ## contribution_table gives the contribution of employer to employee's cpf accounts according to employee's age
 def contribution_table():
+	## 35 and below, above 35 to 45, above 45 to 55 
 	d = {'Age': [35,36,46,51,56,61,66], 
 	'Ordinary': [0.23, 0.21,0.19,0.15,0.12,0.035,0.01],
 	'Special': [0.06, 0.07,0.08,0.115,0.035,0.025,0.01],
@@ -26,6 +27,8 @@ def calculation(salary,num_months,contribution_o,contribution_s,contribution_m):
 ## calculate_each_account gives the contribution to each account according to the salary at the respective age
 def calculate_each_account(df):
 
+	print(df)
+
 
 	# To retrieve the contribution table
 	contribution_percentage=contribution_table()
@@ -46,9 +49,11 @@ def calculate_each_account(df):
 		# To retrieve the first age in the data frame that's larger than or equal to the age concerned
 		first_largest_age=next(row['Age'] for index, row in contribution_percentage.iterrows() if row['Age'] >= df['Age'][i])
 		first_largest_age=int(first_largest_age)
+		print(first_largest_age)
 
 		# To retrieve corresponding percentages for the corresponding age
 		index_first_largest_age=contribution_percentage[contribution_percentage['Age']==first_largest_age].index.item()
+		print(index_first_largest_age)
 		difference=1
 
 		if(df['Age'][i]<first_largest_age):
@@ -60,7 +65,8 @@ def calculate_each_account(df):
 				index_first_largest_age-=1
 
 		# To calculate the corresponding absolute amount contributed to each account for the first year
-		salary=int(df['Salary'][index_first_largest_age])
+		print(index_first_largest_age)
+		salary=int(df['Salary'][i])
 		ordinary,special,medisave=calculation(salary,num_months,contribution_percentage.loc[[index_first_largest_age]]['Ordinary'].values[0],
 			contribution_percentage.loc[[index_first_largest_age]]['Special'].values[0],
 			contribution_percentage.loc[[index_first_largest_age]]['Medisave'].values[0])
@@ -89,6 +95,6 @@ if __name__== "__main__":
 	currentMonth = datetime.now().month
 	print(currentMonth)
 	# To test the code
-	d = {'Age': [34, 45], 'Salary': [3000, 4000]}
+	d = {'Age': [34, 55], 'Salary': [3000, 4000]}
 	df = pd.DataFrame(data=d)
 	calculate_each_account(df)
